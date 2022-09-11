79cecbd0b939f0e3d7433d71be06499647de9184
roshansw <roshanwalzade2@gmail.com>
2022-09-11 17:42:11 +0530

    Added: Functionality to accept Email

--- src/RegexModule/UserRegistration.java
+++ src/RegexModule/UserRegistration.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 
-public class UserRegistration {	
+public class UserRegistration {		
+	Scanner sc;
 	
 	public void nameValidation() {
-		Scanner sc = new Scanner(System.in);		
-		final String textRegexValidation = "(^[A-Z][a-z]{2,})";
+		sc = new Scanner(System.in);		
+		final String textPattern = "(^[A-Z][a-z]{2,})";
 		System.out.print("Enter the FirstName and LastName to validate : ");
         String firstName = sc.nextLine();
         String lastName = sc.nextLine();
         
-        boolean resultForFirstName = Pattern.matches(textRegexValidation, firstName);
-        boolean resultForLastName = Pattern.matches(textRegexValidation, lastName);
+        boolean resultForFirstName = Pattern.matches(textPattern, firstName);
+        boolean resultForLastName = Pattern.matches(textPattern, lastName);
         
         if (resultForFirstName && resultForLastName) {
             System.out.println("First Name is valid and acceptable.");
@@ -26,6 +27,18 @@ public class UserRegistration {
             System.out.println("Enter valid First Name");
             nameValidation();
         }
-	}	
+	}
+	
+	public void eMailValidation () {
+        sc = new Scanner(System.in);
+        final String emailPattern = "[a-zA-Z1-9]+([.]xyz|)+@bl+[.]co([.]in|)";
+        System.out.print("Enter email address to validate : ");
+        String eMail = sc.nextLine();
+        boolean resultForEmailValidation = Pattern.matches(emailPattern, eMail);
+        if (resultForEmailValidation)
+            System.out.println("Email ID is acceptable.");
+        if (!resultForEmailValidation)
+            System.out.println("Email ID is not acceptable.");
+    }
 
 }
--- src/RegexModule/UserValidationMain.java
+++ src/RegexModule/UserValidationMain.java
@@ -4,7 +4,8 @@ public class UserValidationMain {
 
 	public static void main(String[] args) {
 		UserRegistration ur = new UserRegistration();
-		ur.nameValidation();
+//		ur.nameValidation();
+		ur.eMailValidation(); 
 	}
 
 }
