8c7b79e2908262cc198dcbbe65efb5c90e665064
roshansw <roshanwalzade2@gmail.com>
2022-09-11 17:21:28 +0530

    Added: Functionality to accept FirstName starts with Cap and min 3 char

--- src/RegexModule/UserRegistration.java
+++ src/RegexModule/UserRegistration.java
@@ -1,5 +1,23 @@
 package RegexModule;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
-public class UserRegistration {
+
+public class UserRegistration {	
+	
+	public void firstNameValidate() {
+		Scanner sc = new Scanner(System.in);		
+		final String regexFistName = "(^[A-Z][a-z]{3,})";
+		System.out.print("Enter the FirstName to validate : ");
+        String firstName = sc.nextLine();
+        boolean resultForFirstName = Pattern.matches(regexFistName, firstName);
+        if (resultForFirstName) {
+            System.out.println("First Name is valid and acceptable.");
+        }
+        else {
+            System.out.println("Enter valid First Name");
+            firstNameValidate();
+        }
+	}	
 
 }
--- src/RegexModule/UserValidationMain.java
+++ src/RegexModule/UserValidationMain.java
@@ -3,8 +3,8 @@ package RegexModule;
 public class UserValidationMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		UserRegistration ur = new UserRegistration();
+		ur.firstNameValidate();
 	}
 
 }
